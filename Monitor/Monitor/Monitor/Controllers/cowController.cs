54988177f34bb0bc496c225dadc4af5323c2a090
BING <407272759@qq.com>
2015-01-29 20:36:12 +0800

    建立奶牛数据监测系统
    实现了奶牛管理和设备管理，等待添加数据显示的功能

---
+++ Monitor/Monitor/Monitor/Controllers/cowController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Monitor.Models;
+
+namespace Monitor.Controllers
+{
+    public class cowController : Controller
+    {
+        private testEntities db = new testEntities();
+
+        //
+        // GET: /cow/
+
+        public ActionResult Index()
+        {
+            return View(db.cow.ToList());
+        }
+
+        //
+        // GET: /cow/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            cow cow = db.cow.Find(id);
+            if (cow == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cow);
+        }
+
+        //
+        // GET: /cow/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /cow/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(cow cow)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.cow.AsNoTracking().SingleOrDefault(g => g.cowId == cow.cowId);
+                if (item != null && item.id != cow.id)
+                {
+                    ViewBag.error = "奶牛编号重复，请修改";
+                    return View(cow);
+                }
+                db.cow.Add(cow);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cow);
+        }
+
+        //
+        // GET: /cow/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            cow cow = db.cow.Find(id);
+            if (cow == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cow);
+        }
+
+        //
+        // POST: /cow/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(cow cow)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.cow.AsNoTracking().SingleOrDefault(g => g.cowId == cow.cowId);
+                if (item != null && item.id != cow.id)
+                {
+                    ViewBag.error = "奶牛编号重复，请修改";
+                    return View(cow);
+                }
+                db.Entry(cow).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cow);
+        }
+
+        //
+        // GET: /cow/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            cow cow = db.cow.Find(id);
+            if (cow == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cow);
+        }
+
+        //
+        // POST: /cow/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            cow cow = db.cow.Find(id);
+            db.cow.Remove(cow);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Monitor/Monitor/Monitor/Controllers/deviceController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Monitor.Models;
+
+namespace Monitor.Controllers
+{
+    public class deviceController : Controller
+    {
+        private testEntities db = new testEntities();
+
+        //
+        // GET: /device/
+
+        public ActionResult Index()
+        {
+            return View(db.cow_device.ToList());
+        }
+
+        //
+        // GET: /device/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            cow_device cow_device = db.cow_device.Find(id);
+            if (cow_device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cow_device);
+        }
+
+        //
+        // GET: /device/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.cowId = new SelectList(db.cow, "cowId", "cowId");
+            return View();
+        }
+
+        //
+        // POST: /device/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(cow_device cow_device)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                var item = db.cow_device.AsNoTracking().SingleOrDefault(g => g.deviceId == cow_device.deviceId);
+                if (item != null && item.id != cow_device.id)
+                {
+                    ViewBag.cowId = new SelectList(db.cow, "cowId", "cowId");
+                    ViewBag.error = "设备编号重复，请修改";
+                    return View(cow_device);
+                }
+                if (cow_device.cowId != null)
+                {
+                    cow_device device = db.cow_device.AsNoTracking().SingleOrDefault(g => g.cowId == cow_device.cowId);
+                    if (device != null)
+                    {
+                        device.cowId = null;
+                        db.Entry(device).State = EntityState.Modified;
+                    }
+                }
+                
+                db.cow_device.Add(cow_device);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.cowId = new SelectList(db.cow, "cowId", "cowId");
+            return View(cow_device);
+        }
+
+        //
+        // GET: /device/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            cow_device cow_device = db.cow_device.Find(id);
+            if (cow_device == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.cowId = new SelectList(db.cow, "cowId", "cowId",cow_device.cowId);
+            return View(cow_device);
+        }
+
+        //
+        // POST: /device/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(cow_device cow_device)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.cow_device.AsNoTracking().SingleOrDefault(g => g.deviceId == cow_device.deviceId);
+                if (item != null && item.id != cow_device.id)
+                {
+                    ViewBag.cowId = new SelectList(db.cow, "cowId", "cowId");
+                    ViewBag.error = "设备编号重复，请修改";
+                    return View(cow_device);
+                }
+                if (cow_device.cowId != null)
+                {
+                    cow_device device = db.cow_device.AsNoTracking().SingleOrDefault(g => g.cowId == cow_device.cowId);
+                    if (device != null&&device.deviceId!=cow_device.deviceId)
+                    {
+                        device.cowId = null;
+                        db.Entry(device).State = EntityState.Modified;
+                    }
+                }
+                db.Entry(cow_device).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.cowId = new SelectList(db.cow, "cowId", "cowId");
+            return View(cow_device);
+        }
+
+        //
+        // GET: /device/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            cow_device cow_device = db.cow_device.Find(id);
+            if (cow_device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cow_device);
+        }
+
+        //
+        // POST: /device/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            cow_device cow_device = db.cow_device.Find(id);
+            db.cow_device.Remove(cow_device);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
