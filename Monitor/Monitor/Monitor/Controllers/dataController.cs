b0cd61e18d7ef201e35da274febe0381ac55e4ef
BING <407272759@qq.com>
2015-01-30 20:50:15 +0800

    能根据日期和牛号和阈值查询一段时间的数据并显示出来，下一步是给dropdownlist添加事件，用ajax更新图表数据。

--- Monitor/Monitor/Monitor/Controllers/dataController.cs
+++ Monitor/Monitor/Monitor/Controllers/dataController.cs
@@ -18,21 +18,59 @@ namespace Monitor.Controllers
         public ActionResult Index()
         {
             string dataStr = null;
-            var list = db.data.SqlQuery("SELECT * FROM `data` where DATE_SUB(CURDATE(), INTERVAL 7 DAY) <= date(date);").ToList();
-            foreach (var listItem in list)
+            string dateStr = null;
+            string cowId = db.cow.First().cowId;
+            transfer(cowId, 7, 0,out dataStr,out dateStr);
+            if (dataStr != null)
             {
-                dataStr += listItem.value1 + "," + listItem.value2 + "," + listItem.value3 + "," + listItem.value4 + "," + listItem.value5 + "," + listItem.value6 + ","
-                    + listItem.value7 + "," + listItem.value8 + "," + listItem.value9 + "," + listItem.value10 + "," + listItem.value11 + "," + listItem.value12 + ","
-                    + listItem.value13 + "," + listItem.value14 + "," + listItem.value15 + "," + listItem.value16 + "," + listItem.value17 + "," + listItem.value18 + ","
-                    + listItem.value19 + "," + listItem.value20 + "," + listItem.value21 + "," + listItem.value22 + "," + listItem.value23 + "," + listItem.value24 + ",";
+                dataStr = dataStr.Substring(0, dataStr.Length - 1);
             }
-            dataStr=dataStr.Substring(0, dataStr.Length - 1);
+            if (dateStr != null)
+            {
+                dateStr = dateStr.Substring(0, dateStr.Length - 1);
+            }
             ViewBag.data = dataStr;
+            ViewBag.date = dateStr;
             ViewBag.cowId = new SelectList(db.cow, "cowId", "cowId");
             ViewBag.during = new SelectList(new string[] { "7", "14", "30", "37" });
             ViewBag.threshold = new SelectList(new string[] { "0", "1", "2" });
-            ViewBag.grading = new SelectList(new string[] { "1", "2", "4","8" });
+            ViewBag.grading = new SelectList(new string[] { "1", "2", "4", "8" });
             return View();
+        }
+
+        public ContentResult show(string cowId, string during, string threshold, string grading)
+        {
+            string dataStr = null;
+            string dateStr = null;
+            transfer(cowId, int.Parse(during), int.Parse(threshold), out dataStr, out dateStr);
+            return Content("{dataStr: "+dataStr+", datestr: "+dateStr+"}");
+        }
+
+        public void transfer(string cowId,int dayCount,int threshold,out string dataStr,out string dateStr)
+        {
+            DateTime today = DateTime.Now.AddDays(-dayCount+1);
+            dateStr = "";
+            dataStr = "";
+            for (int i = 0, j = 0; i < dayCount; ++i)
+            {
+                string cmd = "SELECT * FROM `data` where date='" + today.ToString("yyyy/MM/dd") + "' AND cowId = " + cowId + " AND threshold = " + threshold + ";";
+                var list = db.data.SqlQuery(cmd).ToList();
+                if (list.Count!=0)
+                {
+                    var listItem = list[0];
+                    dataStr += listItem.value1 + "," + listItem.value2 + "," + listItem.value3 + "," + listItem.value4 + "," + listItem.value5 + "," + listItem.value6 + ","
+                            + listItem.value7 + "," + listItem.value8 + "," + listItem.value9 + "," + listItem.value10 + "," + listItem.value11 + "," + listItem.value12 + ","
+                            + listItem.value13 + "," + listItem.value14 + "," + listItem.value15 + "," + listItem.value16 + "," + listItem.value17 + "," + listItem.value18 + ","
+                            + listItem.value19 + "," + listItem.value20 + "," + listItem.value21 + "," + listItem.value22 + "," + listItem.value23 + "," + listItem.value24 + ",";
+                    dateStr += listItem.date + ",";
+                }
+                else
+                {
+                    dataStr += "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,";
+                    dateStr += today.ToShortDateString() + ",";
+                }
+                today = DateTime.Now.AddDays(-dayCount+2+i);
+            }
         }
 
     }
